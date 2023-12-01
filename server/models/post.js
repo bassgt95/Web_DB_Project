17d69613d08ebef7498807b7e754eb7478bf8180
bassgt95 <bassgt95@gmail.com>
2023-12-01 12:14:35 -0500

    update project for assignment 6

---
+++ server/models/post.js
@@ -0,0 +1,21 @@
+const posts = [
+    {
+        postId: 54321,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, iusto minus eligendi impedit, sit totam facilis rem ullam, ipsa quod fugit sapiente soluta ut excepturi doloribus illo neque laborum rerum.',
+        date: new Date()
+    },
+    {
+        postId: 09271,
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam veritatis hic eum provident rem veniam fugiat deserunt labore minima dignissimos facilis autem, voluptas molestias amet vel explicabo unde est quaerat.',
+        date: new Date()
+    },
+    {
+        postId: 96024,
+        content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum enim impedit hic soluta ipsum, perferendis accusamus nostrum, assumenda est sed natus doloribus? Dignissimos blanditiis libero suscipit cumque nihil, nulla dolores?',
+        date: new Date()
+    }
+]
+
+const getPosts = () => posts;
+
+// const allPosts = JSON.stringify(posts);
---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        userId: 12345,
+        firstName: 'Daphne',
+        lastName: 'Blake',
+        email: 'daphne@mystery.inc',
+        password: 'j33P3r5'
+    },
+    {
+        userId: 17290,
+        userName: 'Velma',
+        lastName: 'Dinkley',
+        email: 'velma@mystery.inc',
+        password: 'j1Nk135'
+    },
+    {
+        userId: 42069,
+        userName: 'Norville',
+        lastName: 'Rogers',
+        email: 'shaggy@mystery.inc',
+        password: 'z01Nk5'
+    }
+]
+
+// const allUsers = JSON.stringify(users);
+const getUsers = () => users;
